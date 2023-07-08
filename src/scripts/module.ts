3ab8daf7e1dca81e02b4c186228b065125fe0b98
Alan Laredo <AlanLaredo@hotmail.com>
2023-07-08 12:34:27 -0600

    feat: initial communication via socket

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,3 +1,26 @@
+import { MODULE_NAME } from "src/constants.ts";
+
+type Payload<Action extends string> = {
+  action: Action;
+};
+export interface ActionRequest extends Payload<"action"> {
+  key: "key";
+}
+
+export type SocketPayload = ActionRequest;
+
 Hooks.once("init", async function () {});
 
-Hooks.once("ready", async function () {});
+Hooks.once("ready", async function () {
+  console.log(`${MODULE_NAME} | Ready`);
+
+  game.socket.on(`module.${MODULE_NAME}`, ({ action, ...payload }) => {
+    switch (action) {
+      default:
+        console.groupCollapsed(`${MODULE_NAME}::${action}`);
+        console.log(payload);
+        console.groupEnd();
+        break;
+    }
+  });
+});
